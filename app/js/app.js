c63e0dfa9c65265dccace83d1d67f8daecc191af
gitMonks <thanksimran@gmail.com>
2017-02-21 08:53:56 +0000

    Designed own solution to multi select checkbox problem

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,33 @@
-console.log("hello world")
+
+const checkboxElements = document.querySelectorAll("input");
+const checkboxesArray = Array.from(checkboxElements);
+
+function multiSelect(event) {
+	//this function will check if there are other checkboxes selected
+	//if yes it will create a range of maximum possible (in case there 
+	//are more than one selected alread) checkboxes 
+	//and select them
+	const selectedIndex = [];
+//	let selected = []
+
+	checkboxesArray.forEach((el, index) => {
+		event.shiftKey && el.checked ? selectedIndex.push(index) : "";
+		// if (event.shiftKey === true) {
+		// 	if(el.checked){
+		// 		selectedIndex.push(index);
+		// 	}
+		// }
+	});
+
+
+	if (selectedIndex.length > 1) {
+		for (let i = selectedIndex[0]; i <= selectedIndex[selectedIndex.length -1]; i++){
+			console.log(checkboxElements[i].checked = true)
+		}
+	}
+}
+	
+
+checkboxesArray.forEach((el) => {
+	el.addEventListener('click', multiSelect)
+})
